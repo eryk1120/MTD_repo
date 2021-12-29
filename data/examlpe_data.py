306bf250eb6f94fcf2b88cddc3d7f43c7caf3762
Eryk Salamoński <eryk1120@gmail.com>
2021-12-29 17:04:48 +0100

    added data generator

---
+++ data/examlpe_data.py
@@ -0,0 +1,22 @@
+class data_generator():
+    '''
+    Class for easy generating data
+    '''
+    przyklad_prezka = [[670, 758, 427, 581, 221, 369], [361, 252, 132, 492, 680], [332, 497, 690, 759], [357, 394, 431],
+                       [391, 650], [521]]
+
+    def __init__(self):
+        pass
+
+    def __del__(self):
+        pass
+
+    def random(self, noConnections):
+        # TODO: zrobić to xDD
+        pass
+
+    def prezka(self):
+        '''
+        returns example from
+        '''
+        return self.przyklad_prezka
