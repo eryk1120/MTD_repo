c545a4617787fb0979fd1008ccc753c695885fb6
Eryk Salamoński <eryk1120@gmail.com>
2022-01-21 01:08:20 +0100

    finished brute

---
+++ src/brute.py
@@ -0,0 +1,38 @@
+import itertools
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from multiprocessing import cpu_count
+from data.example_data import data_generator
+from src.timer_decorator import timeIt
+# from tqdm import tqdm
+import time
+
+
+@timeIt
+def brute(ds):
+    def _calcTotaDistance(permutation):
+        totDist = 0
+        for i in range(len(permutation) - 1):
+            totDist += ds[permutation[i]][permutation[i + 1]]
+        print(totDist)
+        return totDist
+
+    noCheckpoints = len(ds[0])
+    permutations = itertools.permutations([i for i in range(noCheckpoints)], noCheckpoints)
+
+    lowest = sum(ds[0])
+    bestPermutation = list()
+
+    for p in permutations:
+        d = _calcTotaDistance(p)
+        if lowest > d:
+            lowest = d
+            bestPermutation = p
+    print(lowest)
+    print(bestPermutation)
+    return bestPermutation, lowest
+
+    # with ThreadPoolExecutor(max_workers=cpu_count()) as ex:
+    #     futures = [(ex.submit(_calcTotaDistance(i)),i) for i in permutations]
+    #     for d,road in as_completed(futures):
+    #         if lowest > d:
+    #             lowest =d
